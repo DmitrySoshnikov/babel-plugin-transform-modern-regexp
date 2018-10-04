5320bdacfc2a19b57c5bc6255327ca77a2daa643
David Baumgold <david@davidbaumgold.com>
2018-10-04 11:45:40 +0200

    Upgrade to Babel 7

--- __tests__/modern-regexp-test.js
+++ __tests__/modern-regexp-test.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 const path = require('path');
 const plugin = require('..');
 
-const {transformFileSync} = require('babel-core');
+const {transformFileSync} = require('@babel/core');
 
 const fixturesDir = path.join(__dirname, 'fixtures');
 
